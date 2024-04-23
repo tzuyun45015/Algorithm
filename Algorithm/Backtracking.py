667d821a6c71f1464176f20a46d3116cc55ab498
Tzu-Yun, Huang <101507480+tzuyun45015@users.noreply.github.com>
2024-04-23 12:15:56 +0800

    Update Backtracking.py

--- Algorithm/Backtracking.py
+++ Algorithm/Backtracking.py
@@ -18,14 +18,17 @@ class Solution:
         return res
 
 result = []
-def backtrack(路径, 选择列表):
+def backtrack(路径):
     if 满足结束条件:
         result.add(路径)
         return
     
-    for 选择 in 选择列表:
-        做选择
-        backtrack(路径, 选择列表)
-        撤销选择
+
+    做选择
+    backtrack(路径表)
+    另一個选择
+    backtrack(路徑)
+backtrack(0)
+return res
 
         
