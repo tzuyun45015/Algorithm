d3e65e7f633d6eda7db829fc1732ce1de51efc2e
Tzu-Yun, Huang <101507480+tzuyun45015@users.noreply.github.com>
2024-04-24 14:48:05 +0800

    Update HashSet.py

--- Algorithm/HashSet.py
+++ Algorithm/HashSet.py
@@ -1,10 +1,35 @@
 #Time O(n)
 #Space O(n)
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
+class ListNode:
+    def __init__(self,key):
+        self.key = key
+        self.next = None
+
+class MyHashSet:
+    def __init__(self):
+        self.hashset = [ListNode(0) for i in range(10**4)] # less will have more collision 
+
+    def add(self, key: int) -> None:
+        index = key % len(self.hashset)
+        cur = self.hashset[index]
+        while cur.next:
+            if cur.next.key == key:
+                return
+            cur = cur.next
+        cur.next = ListNode(key)
+
+    def remove(self, key: int) -> None:
+        cur = self.hashset[key % len(self.hashset)]
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next      
+
+    def contains(self, key: int) -> bool:
+        cur = self.hashset[key % len(self.hashset)]
+        while cur.next:
+            if cur.next.key == key:
+                return True    
+            cur = cur.next 
         return False
