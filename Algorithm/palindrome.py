3eb5f975b62d82b54cdc4c8af43e1c627894a5f9
Tzu-Yun, Huang <101507480+tzuyun45015@users.noreply.github.com>
2024-05-04 14:54:46 +0800

    Create  palindrome.py

---
+++ Algorithm/palindrome.py
@@ -0,0 +1,6 @@
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""
+        for i in s:
+            if i.isalnum():
+                newStr += i.lower()
+        return newStr == newStr[::-1]
