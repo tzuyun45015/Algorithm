266e1016061c7c90251a5a05548e4630543ce3ba
Tzu-Yun, Huang <101507480+tzuyun45015@users.noreply.github.com>
2024-05-06 16:45:45 +0800

    Update BinarySearchTree.py

--- Algorithm/BinarySearchTree.py
+++ Algorithm/BinarySearchTree.py
@@ -1,1 +1,14 @@
-
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def BST(s, left, right):
+            if not s:
+                return True
+            if not (s.val < right and s.val > left):
+                return False
+            return (BST(s.left, left, s.val) and
+                    BST(s.right, s.val, right))
+                    
+        return BST(root, float("-inf"), float("inf"))
