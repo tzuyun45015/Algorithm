26162fc3aa10c275278b12db8adb8dc469367ead
Tzu-Yun, Huang <101507480+tzuyun45015@users.noreply.github.com>
2024-05-25 17:30:30 +0800

    Update InsertionSort.py

--- Hackerrank/InsertionSort.py
+++ Hackerrank/InsertionSort.py
@@ -10,3 +10,15 @@ def insertionSort1(n, arr):
         else:
             break
     print(*arr)
+
+def insertionSort2(n, arr):
+    # Write your code here
+    l, r = 0, 0
+    for i in range(1,n):
+        tmp = arr[i]
+        index = i
+        while arr[index-1] > tmp and index > 0:
+            arr[index] = arr[index-1]
+            index -=1
+        arr[index] = tmp
+        print (" ".join([str(a) for a in arr]))
