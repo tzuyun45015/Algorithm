89f50b04400a2d98d4154645cec55649e763d0f7
Tzu-Yun, Huang <101507480+tzuyun45015@users.noreply.github.com>
2024-05-31 11:16:55 +0800

    Update LCA.py

--- Hackerrank/DataStructure/LCA.py
+++ Hackerrank/DataStructure/LCA.py
@@ -1,1 +1,9 @@
-
+def LCA(root,v1,v2):
+  cur = root
+  while cur:
+    if v1 < cur.info and v2 < cur.info:
+      cur = cur.left
+    elif v1 > cur.info and v2 > cur.info:
+      cur = cur.right
+    else:
+      return cur
